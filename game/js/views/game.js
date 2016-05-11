0d73a01e9972b402f57abaa22907d4fc87a7ab09
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-05-11 21:59:44 +1000

    :straight_ruler: resize pixi canvas/renderer based on screen size. Still need to apply a global scale to all sprites to fit the canvas size

--- game/js/views/game.js
+++ game/js/views/game.js
@@ -1,91 +1,74 @@
 'use strict';
 
-var PIXI = require('pixi.js');
+import PIXI from 'pixi.js';
+import {sortChildren, resizeStage, resizeRenderer} from './pixi';
+import {on} from 'ensemblejs/lib/events';
 
-import {sortChildren} from './pixi';
-
-function boardIsSmallerThenScreen(boardDimensions, screenDimensions) {
-  return (boardDimensions.width < screenDimensions.usableWidth ||
-      boardDimensions.height < screenDimensions.usableHeight);
-}
-
-function boardIsLargerThanScreen(boardDimensions, screenDimensions) {
-  return !boardIsSmallerThenScreen(boardDimensions, screenDimensions);
-}
-
-function calculateOffset (boardDimensions, screenDimensions) {
-  if (boardIsSmallerThenScreen(boardDimensions, screenDimensions)) {
-    return {
-      x: (screenDimensions.usableWidth - boardDimensions.width) / 2,
-      y: (screenDimensions.usableHeight - boardDimensions.height) / 2
-    };
-  }
-
-  return { x: 0, y: 0 };
-}
-
-function scaleBoard (dims, config) {
-  if (boardIsLargerThanScreen(config.pacman.board, dims)) {
-    let ratio = Math.min(
-      dims.screenWidth/config.pacman.board.width,
-      dims.screenHeight/config.pacman.board.height
-    );
-
-    return {
-      x: ratio,
-      y: ratio
-    };
-  }
-
-  return {
-    x: 1.0,
-    y: 1.0
-  };
-}
-
-// var define = require('ensemblejs/define');
 // var state = require('ensemblejs/state');;
 
 //jshint maxparams:false
 module.exports = {
   type: 'OnClientReady',
-  deps: ['Config', 'StateTracker', 'DefinePlugin', '$', 'DeviceMode'],
-  func: function View (config, tracker, define, $, deviceMode) {
+  deps: ['Config', 'StateTracker', 'Window'],
+  func: function View (config, tracker, window) {
 
-    var scalingFactor = 1;
-    var setupScreenAsSharedScreen = require('./shared');
-    var setupScreenAsPacman = require('./pacman');
-    var setupScreenAsBlinky = require('./blinky');
-    var setupScreenAsPinky = require('./pinky');
-    var setupScreenAsInky = require('./inky');
-    var setupScreenAsClyde = require('./clyde');
-    var setupScreenAsWaitingRoom = require('./waiting-room');
+    let setupScreenAsSharedScreen = require('./shared');
+    let setupScreenAsPacman = require('./pacman');
+    // let setupScreenAsBlinky = require('./blinky');
+    // let setupScreenAsPinky = require('./pinky');
+    // let setupScreenAsInky = require('./inky');
+    // let setupScreenAsClyde = require('./clyde');
+    // let setupScreenAsWaitingRoom = require('./waiting-room');
+    let stage;
+    let renderer;
 
-    var stage;
-    var offset;
-    var scale;
+    on('RenderFrame', function OnRenderFrame () {
+      return function renderScene () {
+        if (!renderer) {
+          return;
+        }
+
+        renderer.render(stage);
+      };
+    });
+
+    function resize (dims) {
+      const board = {
+        width: config().pacman.board.width,
+        height: config().pacman.board.height
+      };
+      const screen = {
+        width: dims.screenWidth,
+        height: dims.screenHeight
+      };
+      const usable = {
+        width: dims.usableWidth,
+        height: dims.usableHeight
+      };
+
+      resizeStage(stage, board, screen, usable);
+      resizeRenderer(renderer, usable);
+    }
+
+    on('Resize', function OnResize () { return resize; });
+    // on('Resize', ['Dep'], dep => resize(dep)]);
+    // on('Resize', resize); is on('Resize', [], () => resize()]);
+
     return function setup (dims, playerNumber) {
-      scalingFactor = dims.usableWidth / 280;
-
-      let renderer;
-
       PIXI.loader
         .add('/game/assets/images/pacman.json')
         .add('/game/assets/images/ghost.json')
         .load(function () {
           stage = new PIXI.Container();
           renderer = PIXI.autoDetectRenderer(dims.usableWidth, dims.usableHeight);
-          $()('#' + config().client.element).append(renderer.view);
 
-          offset = calculateOffset(config().pacman.board, dims);
-          stage.position.x = offset.x;
-          stage.position.y = offset.y;
-          scale = scaleBoard(dims, config());
-          stage.scale.x = scale.x;
-          stage.scale.y = scale.y;
+          const parent = window().document.querySelector(`#${config().client.element}`);
+          parent.appendChild(renderer.view);
+
+          resize(dims);
 
           // if (deviceMode() === 'observer') {
-            setupScreenAsSharedScreen(dims, stage, tracker);
+          setupScreenAsSharedScreen(dims, stage, tracker);
           // }
 
           tracker().onChangeTo('players:' + playerNumber + '.pacman.role', 'pacman', setupScreenAsPacman, [stage, tracker]);
@@ -97,16 +80,6 @@ module.exports = {
 
           sortChildren(stage);
         });
-
-      define()('OnRenderFrame', function OnRenderFrame () {
-        return function renderScene () {
-          if (!renderer) {
-            return;
-          }
-
-          renderer.render(stage);
-        };
-      });
     };
   }
 };
--- game/js/views/pixi.js
+++ game/js/views/pixi.js
@@ -11,3 +11,64 @@ export function getTextures(items) {
     return PIXI.Texture.fromFrame(item, undefined, PIXI.SCALE_MODES.NEAREST);
   });
 }
+
+function boardIsSmallerThenScreen(board, usable) {
+  return (board.width < usable.width || board.height < usable.height);
+}
+
+function boardIsLargerThanScreen(board, usable) {
+  return !boardIsSmallerThenScreen(board, usable);
+}
+
+export function calculateOffset (board, usable) {
+  if (boardIsSmallerThenScreen(board, usable)) {
+    return {
+      x: (usable.width - board.width) / 4,
+      y: (usable.height - board.height) / 4
+    };
+  }
+
+  return { x: 0, y: 0 };
+}
+
+export function calculateScale (board, screen, usable) {
+  if (boardIsLargerThanScreen(board, usable)) {
+    // TODO: can we use ratio from somewhere else?
+    let ratio = Math.min(
+      screen.width/board.width,
+      screen.height/board.height
+    );
+
+    return {
+      x: ratio,
+      y: ratio
+    };
+  }
+
+  return {
+    x: 1.0,
+    y: 1.0
+  };
+}
+
+export function resizeRenderer (renderer, usable) {
+  if (!renderer) {
+    return;
+  }
+
+  renderer.resize(usable.width, usable.height);
+}
+
+export function resizeStage (stage, board, screen, usable) {
+  if (!stage) {
+    return;
+  }
+
+  const scale = calculateScale(board, screen, usable);
+  stage.scale.x = scale.x;
+  stage.scale.y = scale.y;
+
+  const offset = calculateOffset(board, usable);
+  stage.position.x = offset.x;
+  stage.position.y = offset.y;
+}
